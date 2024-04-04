911b346936abd27f6eb5ed85971d8647b6d43457
Ishan sharma <ishansharma000170@gmail.com>
2024-04-05 00:03:22 +0530

    G

--- calculator.c
+++ calculator.c
@@ -1,27 +1,32 @@
-#include<stdio.h>
-int main(){
-    double firstNumber, secondNumber;
-    char op;
-    printf("Enter the first number : ");
-    scanf("%lf",&firstNumber);
-    printf("Enter the second number : ");
-    scanf("%lf",&secondNumber);
-    printf("Enter the operation");
-    printf("\nFor addition + ");
-    printf("\nFor subtraction - ");
-    printf("\nFor multiplication * ");
-    printf("\nFor division / : ");
-    scanf(" %c",&op);
-    if(op == '+') {
-        printf("The summation of %lf and %lf = %lf",firstNumber,secondNumber,(firstNumber+secondNumber));
-    }else if(op == '-') {
-        printf("The subtraction of %lf and %lf = %lf",firstNumber,secondNumber,(firstNumber-secondNumber));
-    }else if(op == '*') {
-        printf("The multiplication of %lf and %lf = %lf",firstNumber,secondNumber,(firstNumber*secondNumber));
-    }else if(op == '/') {
-        printf("The division of %lf and %lf = %lf",firstNumber,secondNumber,(firstNumber/secondNumber));
-    }else{
-        printf("Invalid Input");
-    }
-    return 0;
+#include <stdio.h>
+
+int main() {
+
+  char op;
+  double first, second;
+  printf("Enter an operator (+, -, *, /): ");
+  scanf("%c", &op);
+  printf("Enter two operands: ");
+  scanf("%lf %lf", &first, &second);
+
+  switch (op) {
+    case '+':
+      printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+      break;
+    case '-':
+      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+      break;
+    case '*':
+      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+      break;
+    case '/':
+      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+      break;
+    // operator doesn't match any case constant
+    default:
+      printf("Error! operator is not correct");
+  }
+
+  return 0;
 }
+
